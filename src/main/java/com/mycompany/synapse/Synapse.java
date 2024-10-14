00bc6f0974cf294288a4b08a160b3e6b09d565f9
pkmelo7 <patrick.gregorio2022@gmail.com>
2024-10-13 21:04:14 -0300

    Primeiro Upload do Projeto

---
+++ src/main/java/com/mycompany/synapse/Synapse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.synapse;
+
+/**
+ *
+ * @author pkmelo7
+ */
+public class Synapse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
